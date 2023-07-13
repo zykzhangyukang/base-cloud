570bc63f7ef9a075c0fe13e14c0b52e0b80932c7
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2023-07-13 22:58:45 +0800

    代码优化+2

--- base-auth/base-auth-web/src/main/java/com/coderman/auth/controller/user/UserController.java
+++ base-auth/base-auth-web/src/main/java/com/coderman/auth/controller/user/UserController.java
@@ -9,6 +9,8 @@ import com.coderman.auth.vo.user.UserAssignVO;
 import com.coderman.auth.vo.user.UserLoginRespVO;
 import com.coderman.auth.vo.user.UserPermissionVO;
 import com.coderman.auth.vo.user.UserVO;
+import com.coderman.erp.util.AuthUtil;
+import com.coderman.erp.vo.AuthUserVO;
 import com.coderman.swagger.annotation.ApiReturnParam;
 import com.coderman.swagger.annotation.ApiReturnParams;
 import com.coderman.swagger.constant.SwaggerConstant;
@@ -20,6 +22,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author coderman
